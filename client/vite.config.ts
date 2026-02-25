7b7f69f9e77375b90988d3f2c0d70bd0313100cf
Abbie Seale <114274104+aseale1@users.noreply.github.com>
2026-02-25 11:43:37 -0700

    Add vercel rewrite rule

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+    build: {
+    outDir: "dist"
+  }, 
+   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
