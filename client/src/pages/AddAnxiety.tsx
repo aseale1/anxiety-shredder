8d8bb74ec3df2be70ac6c8b0098f7a7c9e6f9c8b
Abbie Seale <114274104+aseale1@users.noreply.github.com>
2026-04-03 17:13:10 -0600

    Package updates and debugging stale build

--- client/src/pages/AddAnxiety.tsx
+++ client/src/pages/AddAnxiety.tsx
@@ -346,7 +346,7 @@ const AddAnxiety: React.FC = () => {
         <div className="min-h-screen w-screen bg-mountain bg-cover bg-center bg-fixed flex justify-center items-start relative p-8">
         <div className="absolute min-h-full inset-0 bg-black bg-cover opacity-50"></div>
         <div className="relative w-full max-w-6xl bg-amber-50 rounded-lg p-8 m-4">
-        <h1 className="text-black text-center mb-4 pt-8">What's making you feel anxious?</h1>
+        <h1 className="text-black text-center mb-4 pt-8">Testing 7</h1>
         <div className="border-b-2 border-black mb-6"></div>
 
             {/* Display untracked anxieties */}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    build: {
-    outDir: "../client"
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173
+    }
+  },
+  build: {
+    outDir: "dist"
   }
 })
 
